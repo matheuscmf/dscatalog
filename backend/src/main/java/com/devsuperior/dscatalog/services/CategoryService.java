4eb225317d91a6eb93bc5234e39cf6a59190abdb
Matheus Marques Albuquerque <matheuscmf1995@gmail.com>
2024-03-18 21:42:08 -0300

    add seeding database and DTO

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,10 +1,13 @@
 package com.devsuperior.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
 
@@ -14,8 +17,11 @@ public class CategoryService {
 	@Autowired 										// dependência válida do CategoryRepository
 	private CategoryRepository repository;
 	
-	public List<Category> findAll(){
+	@Transactional(readOnly = true)					// garantir transação e evitando lock
+	public List<CategoryDTO> findAll(){
+		List<Category>  list = repository.findAll();				// achar todas as categorias no BD
 		
-		return repository.findAll();				// achar todas as categorias no BD
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+		
 	}
 }
